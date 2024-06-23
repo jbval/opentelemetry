902e42cada27120402332ef053c61feca9a3c19c
jbval <jbval2@gmail.com>
2024-06-23 21:14:59 +0200

    feat: send to OTLP + doc

--- Program.cs
+++ Program.cs
@@ -20,7 +20,11 @@ builder.Services.AddOpenTelemetry()
       .ConfigureResource(resource => resource.AddService(serviceName))
       .WithTracing(tracing => tracing
           .AddAspNetCoreInstrumentation()
-          .AddConsoleExporter())
+          .AddConsoleExporter()
+          .AddOtlpExporter()
+          )
+
+
       .WithMetrics(metrics => metrics
           .AddAspNetCoreInstrumentation()
           .AddConsoleExporter());
@@ -35,8 +39,12 @@ app.Run();
 int GetRandomNumber([FromServices] ILogger<Program> logger){
     logger.LogInformation("Random");
 
-    var random=Random.Shared.Next(0,10);
+    var random = Generate();
     logger.LogInformation("End Random", random);
     return random; 
     
+}
+
+int Generate(){
+return Random.Shared.Next(0,10);
 }
--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # opentelemetry
+
+Add OpenTelemetry :
+
+```shell
+dotnet add package OpenTelemetry.Extensions.Hosting 
+dotnet add package OpenTelemetry.Instrumentation.AspNetCore
+dotnet add package OpenTelemetry.Exporter.Console
+```
+
+
+
+Add OTLP package :
+
+```shell 
+dotnet add package OpenTelemetry.Exporter.OpenTelemetryProtocol
+```
+
+Start Jaeger for demo : 
+```shell
+docker run --rm -e COLLECTOR_ZIPKIN_HOST_PORT=:9411  -p 16686:16686 -p 4317:4317 -p 4318:4318 -p 9411:9411  jaegertracing/all-in-one:latest
+```
