1edea5a0dc89f38d3dbc0dcb1403c447cd69a125
jbval <jbval2@gmail.com>
2024-06-23 20:53:33 +0200

    feat: integration opentelemetry

--- Program.cs
+++ Program.cs
@@ -1,6 +1,31 @@
 using Microsoft.AspNetCore.Mvc;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 
 var builder = WebApplication.CreateBuilder(args);
+
+const string serviceName = "demo-random";
+
+builder.Logging.AddOpenTelemetry(options =>
+{
+    options
+        .SetResourceBuilder(
+            ResourceBuilder.CreateDefault()
+                .AddService(serviceName))
+        .AddConsoleExporter();
+});
+builder.Services.AddOpenTelemetry()
+      .ConfigureResource(resource => resource.AddService(serviceName))
+      .WithTracing(tracing => tracing
+          .AddAspNetCoreInstrumentation()
+          .AddConsoleExporter())
+      .WithMetrics(metrics => metrics
+          .AddAspNetCoreInstrumentation()
+          .AddConsoleExporter());
+
+
 var app = builder.Build();
 
 app.MapGet("/",  GetRandomNumber);
